875f10ab1bc502a73e271c407138511e37b90fba
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-05-06 20:06:30 +0200

    fix: Add Reddit patch exception message for users who ignore the Manager update prompt

--- patches/src/main/kotlin/app/morphe/patches/all/misc/updates/CheckPatcherUpToDatePatch.kt
+++ patches/src/main/kotlin/app/morphe/patches/all/misc/updates/CheckPatcherUpToDatePatch.kt
@@ -1,24 +1,25 @@
 package app.morphe.patches.all.misc.updates
 
-import app.morphe.patcher.Fingerprint
-import app.morphe.patcher.methodCall
+import app.morphe.patcher.Match
+import app.morphe.patcher.patch.BytecodePatchContext
 import app.morphe.patcher.patch.bytecodePatch
 
 // TODO: Delete this after Manager 1.17.0 has been released
 internal val checkPatcherUpToDatePatch = bytecodePatch {
     execute {
         try {
-            Fingerprint(
-                filters = listOf(
-                    methodCall(definingClass = "Lapp/morphe/extension/shared/Utils;")
-                )
-            ).instructionMatches.first().getMethodCalled()
-        } catch (_: Throwable) {
+            //noinspection CheckResult
+            Match.InstructionMatch::class.java.getDeclaredMethod(
+                "getMethodCalled",
+                BytecodePatchContext::class.java
+            )
+        } catch (ex: NoSuchMethodException) {
             throw RuntimeException(
                 "\n\n#####################################\n\n" +
-                        "Your Morphe app is outdated. Please update Morphe " +
-                        "by downloading from https://morphe.software\n\n" +
+                        "Your Morphe app is outdated." +
+                        "\n\nPlease update Morphe by downloading from https://morphe.software\n\n" +
                         "#####################################\n\n"
+                        + ex
             )
         }
     }
